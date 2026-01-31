9128c239af159020bdf15538a8d852a162f87062
Oleg Tamanov <olegtamanov@gmail.com>
2026-01-31 19:11:16 +0300

    add JSONSaver class for storing vacancies in .json

---
+++ src/savers/json_saver.py
@@ -0,0 +1,112 @@
+import json
+from typing import cast
+
+from src.savers.base_saver import BaseSaver
+
+
+class JSONSaver(BaseSaver):
+    """
+    Сохраняет вакансии в JSON-файл.
+    Наследуется от BaseSaver, использует кэш URL для предотвращения дубликатов.
+    """
+
+    _extension = "json"
+
+    def __init__(self, filename: str = "vacancies") -> None:
+        """
+        Инициализирует объект для работы с JSON.
+
+        :param filename: Имя файла без расширения .json
+
+        :raise TypeError: Если имя файла не строка.
+        """
+        super().__init__(filename)
+
+    def get_all_vacancies(self) -> list[dict]:
+        """
+        Возвращает все вакансии из JSON-файла.
+
+        :return: Список словарей с вакансиями.
+
+        :raises FileNotFoundError: Если файл не найден.
+        :raises ValueError: Если файл повреждён или битый JSON.
+        """
+        try:
+            with open(self._path_to_file, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                data = cast(list[dict], data)
+                return data
+
+        except FileNotFoundError:
+            raise FileNotFoundError("Файл не найден или удален")
+
+        except json.JSONDecodeError as error:
+            raise ValueError("Файл поврежден или пуст") from error
+
+    def add_vacancy(self, vacancy: dict[str, str | int]) -> None:
+        """
+        Добавляет вакансию в JSON-файл.
+
+        :param vacancy: Словарь с данными о вакансии.
+
+        :raises ValueError: Если структура данных вакансии неверна.
+        """
+        if "Ссылка на вакансию" not in vacancy or len(vacancy) != 7:
+            raise ValueError("Ошибка в структуре данных вакансии")
+
+        vacancy_url = vacancy["Ссылка на вакансию"]
+        if vacancy_url in self._url_cache:
+            return None
+
+        try:
+            with open(self._path_to_file, "r+", encoding="utf-8") as file:
+                data = json.load(file)
+
+                data.append(vacancy)
+                self._url_cache.add(vacancy_url)
+
+                file.seek(0)
+                file.truncate()
+                json.dump(data, file, indent=4, ensure_ascii=False)
+
+        except (FileNotFoundError, json.JSONDecodeError):
+            with open(self._path_to_file, "w", encoding="utf-8") as file:
+                json.dump([vacancy], file, indent=4, ensure_ascii=False)
+                self._url_cache.add(vacancy_url)
+
+    def delete_vacancy(self, vacancy: dict[str, str | int]) -> None:
+        """
+        Удаляет вакансию из JSON-файла.
+
+        :param vacancy: Словарь с данными вакансии.
+
+        :raises ValueError: Если структура данных неверна.
+        :raises FileNotFoundError: Если файл не найден.
+        :raises ValueError: Если файл повреждён.
+        """
+        if "Ссылка на вакансию" not in vacancy or len(vacancy) != 7:
+            raise ValueError("Ошибка в структуре данных вакансии")
+
+        vacancy_url = vacancy["Ссылка на вакансию"]
+        if vacancy_url not in self._url_cache:
+            return None
+
+        try:
+            with open(self._path_to_file, "r+", encoding="utf-8") as file:
+                data = json.load(file)
+
+                for v in data:
+                    if v.get("Ссылка на вакансию") == vacancy_url:
+                        data.remove(v)
+                        break
+
+                file.seek(0)
+                file.truncate()
+                json.dump(data, file, indent=4, ensure_ascii=False)
+                self._url_cache.remove(vacancy_url)
+
+        except FileNotFoundError:
+            raise FileNotFoundError("Файл не найден или удален")
+
+        except json.JSONDecodeError as error:
+            raise ValueError("Файл поврежден или пуст") from error
